45998f75c0418500666cc31ffa06128202a6cc84
MarkDemidovs <markdemidovs@proton.me>
2026-02-03 21:41:38 +0200

    code refactoring

--- server/src/controllers/notes.ts
+++ server/src/controllers/notes.ts
@@ -1,22 +1,40 @@
 import { Request, Response } from "express";
 import pool from "../db";
 
-export const getNotes = async (req: Request, res: Response) => {
-    try {
-        const { rows } = await pool.query("SELECT * FROM notes");      
-    } catch (error) {
-        res.status(500).json({ error: "Something went wrong in the controller."});
-    }
+interface CreateNoteBody {
+  title: string;
+  content: string;
 }
-export const createNote = async (req: Request, res: Response) => {
-    const { title, content } = req.body;
 
-    try {
-        const { rows } = await pool.query(
-            "INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING *",
-            [title, content]
-        );
-    } catch (error) {
-        res.status(500).json({ error: "insert failed" });
-    }
-}
+export const getNotes = async (_req: Request, res: Response) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM notes");
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({
+      error: "Something went wrong in the controller.",
+    });
+  }
+};
+
+export const createNote = async (
+  req: Request<{}, {}, CreateNoteBody>,
+  res: Response
+) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({ error: "Title and content are required" });
+  }
+
+  try {
+    const { rows } = await pool.query(
+      "INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING *",
+      [title, content]
+    );
+
+    res.status(201).json(rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: "Insert failed" });
+  }
+};
