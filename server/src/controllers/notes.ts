cc00f48a1f802907efcf61c78abaa7de9c094c2f
MarkDemidovs <markdemidovs@proton.me>
2026-02-05 22:38:24 +0200

    notes controller titleRenameNote

--- server/src/controllers/notes.ts
+++ server/src/controllers/notes.ts
@@ -22,3 +22,22 @@ export const createNote = async (req: Request, res: Response) => {
         res.status(500).json({ error: "insert failed" });
     }
 }
+
+export const titleRenameNote = async (req: Request, res: Response) => {
+    const { newTitle} = req.body;
+    const { id } = req.params;
+
+    try {
+        const { rows } = await pool.query(
+            "UPDATE notes SET title = $1 WHERE id = $2 RETURNING *",
+            [newTitle, id]
+        );
+
+        if (rows.length === 0 ) {
+            return res.status(404).json({ error: "Note not found!" });
+        }
+        res.status(200).json(rows[0])
+    } catch (error) {
+        res.status(500).json({ error: "title rename failed ";})
+    }
+}
