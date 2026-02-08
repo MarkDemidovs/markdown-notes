5c07c4ef7e7c28f3e21f160f8063d47c82fbf9ac
MarkDemidovs <markdemidovs@proton.me>
2026-02-08 20:11:10 +0200

    all the necessary stuff for frontend..

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,38 @@
-export default function App() {
+import { useEffect, useState } from "react";
+import type { Note } from "./types/note";
+import { getNotes } from "./api/notes";
+
+export default function NotesPage() {
+
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function load() {
+      try {
+        const data = await getNotes();
+        setNotes(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    load();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
-    <h1 className="text-sm font-semibold text-blue-600 p-6">
-        markdown
-    </h1>
-  )
+    <div>
+      {notes.map(note => (
+        <div key={note.id}>
+          <h3>{note.title}</h3>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+
 }
-
-
-
-
-
-
-
---
+++ client/src/api/notes.ts
@@ -0,0 +1,26 @@
+import type { Note } from "../types/note"
+
+async function apiFetch<T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> {
+
+  const res = await fetch(`http://localhost:4000${url}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(options?.headers || {})
+    },
+    ...options
+  });
+
+  if (!res.ok) {
+    throw new Error("API error");
+  }
+
+  return res.json();
+}
+
+
+export function getNotes(): Promise<Note[]> {
+  return apiFetch<Note[]>("/notes");
+}
