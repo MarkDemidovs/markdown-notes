692e0ece503217b4dc2e76ac67110e917797f903
MarkDemidovs <markdemidovs@proton.me>
2026-02-12 16:41:25 +0200

    new note creation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import type { NoteType } from "./types/note";
-import { getNotes } from "./api/notes";
+import { getNotes, createNote } from "./api/notes";
 import Note from "./Note";
 
 export default function NotesPage() {
@@ -23,13 +23,22 @@ export default function NotesPage() {
     load();
   }, []);
 
+  async function createNoteFunction(title: string, content: string) {
+    try {
+      const data = await createNote(title, content);
+      setNotes(prev => [...prev, data]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   if (loading) return <div>Loading...</div>;
 
   return (
     <div className="p-8">
       <div className="flex items-center justify-between mb-6">
         <h1 className="text-3xl font-bold mb-6">Notes</h1>
-        <button className="rounded shadow border border-gray-300 p-2 w-8 h-8 flex items-center justify-center">+</button>
+        <button onClick={() => createNoteFunction("New Note", "This is a new note.")} className="rounded shadow border border-gray-300 p-2 w-8 h-8 flex items-center justify-center">+</button>
       </div>
       <div className="gap-6 flex">
         {notes.map(note => (
