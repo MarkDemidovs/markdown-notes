8c9bef4439676724abf2d4cd0d52b4e628f0d534
MarkDemidovs <markdemidovs@proton.me>
2026-02-14 19:26:06 +0200

    edit mode frontend

--- client/src/Note.tsx
+++ client/src/Note.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 interface NoteProps {
     title: string;
     content: string;
 }
 
 export default function Note({ title, content }: NoteProps) {
+    const [isEditing, setIsEditing] = useState(false);
+
     return (
         <div className="bg-yellow-100 p-4 rounded shadow w-64 h-40">
-            <h3 className="font-bold text-lg">{title}</h3>
-            <p className="text-gray-700">{content}</p>
+            {isEditing ? (
+                <div className="pb-2">
+                    <div className="flex justify-between items-center mb-2">
+                        <input className="border border-gray-400 rounded p-1 w-full mr-2 bg-yellow-100" defaultValue={title}></input>
+                        <button onClick={() => setIsEditing(false)}>Done</button>
+                    </div>
+                    <textarea className="border border-gray-400 rounded p-1 w-full h-20 bg-yellow-100" defaultValue={content}></textarea>
+                </div>
+            ) : (
+                <div className="cursor-pointer" onClick={() => setIsEditing(true)}>
+                    <h3 className="font-bold text-lg">{title}</h3>
+                    <p className="text-gray-700">{content}</p>
+                </div>
+            )}
         </div>
     )
 }
